80cada955441c90cd366dea180d60ed2fb65da15
Quint Burkley <quint@quint5.tech>
2019-11-17 16:48:11 -0500

    Updated apks for various apps.

--- GmsCore/Android.mk
+++ GmsCore/Android.mk
@@ -11,7 +11,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := GmsCore
-LOCAL_SRC_FILES := GmsCore-v0.2.9.19420-5-cfc90b4.apk
+LOCAL_SRC_FILES := com.google.android.gms-19420020.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
@@ -19,4 +19,3 @@ LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_OVERRIDES_PACKAGES := com.qualcomm.location
 LOCAL_REQUIRED_MODULES := privapp-permissions-com.google.android.gms.xml
 include $(BUILD_PREBUILT)
-
