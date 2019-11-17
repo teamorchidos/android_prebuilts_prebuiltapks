0a55a920e33b2c674c8e47cec6dfbd064acc1ea1
Quint Burkley <quint@quint5.tech>
2019-11-17 11:28:49 -0500

    Replace Aurora Droid back to F-Droid.

--- additional_repos/Android.mk
+++ additional_repos/Android.mk
@@ -4,7 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := additional_repos.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/com.aurora.adroid
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/org.fdroid.fdroid
 LOCAL_SRC_FILES := additional_repos.xml
 include $(BUILD_PREBUILT)
-
