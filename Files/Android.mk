5a0bfa5c3ea9867913b89002d97a5dcd7dded521
Travis CI <deploy@travis-ci.org>
2019-12-05 02:00:37 +0100

    ci - update application Files with me.zhanghai.android.files

--- Files/Android.mk
+++ Files/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Files
-LOCAL_SRC_FILES := me.zhanghai.android.files_12.apk
+LOCAL_SRC_FILES := me.zhanghai.android.files_15.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
