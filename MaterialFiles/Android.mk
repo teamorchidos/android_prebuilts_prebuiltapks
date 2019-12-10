5f12b2740c788e12109b0db953abb62f3ef5b9d7
quint@quint5.tech <quint@quint5.tech>
2019-12-10 18:35:15 -0500

    Rename Files to Material Files.

--- MaterialFiles/Android.mk
+++ MaterialFiles/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := Files
+LOCAL_MODULE := MaterialFiles
 LOCAL_SRC_FILES := me.zhanghai.android.files_15.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
