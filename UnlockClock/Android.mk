9eca69a1e8b0b9291ab2899a2a27d81a621667f9
qwow5 <quint@quint5.tech>
2020-05-07 16:14:25 -0400

    Updated UnlockClock

--- UnlockClock/Android.mk
+++ UnlockClock/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := UnlockClock
-LOCAL_SRC_FILES := unlock_clock_release.apk
+LOCAL_SRC_FILES := com.betterlineage.unlockclock_2.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
