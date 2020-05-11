9d8a429f8a542d5f04a91589dd81fb4b8947ae65
qwow5 <quint@quint5.tech>
2020-05-10 20:54:59 -0400

    Fixed privapp permissions for core apps.

--- GmsCore/Android.mk
+++ GmsCore/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := privapp-permissions-com.google.android.gms.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
