3e95ba70cf391e3034282417bdc1cf646e3b2c86
qwow5 <quint@quint5.tech>
2020-05-13 12:07:13 -0400

    Revert "Fixed privapp permissions for core apps."

--- FDroidPrivilegedExtension/Android.mk
+++ FDroidPrivilegedExtension/Android.mk
@@ -4,7 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := privapp-permissions-org.fdroid.fdroid.privileged.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
--- GmsCore/Android.mk
+++ GmsCore/Android.mk
@@ -4,7 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := privapp-permissions-com.google.android.gms.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
