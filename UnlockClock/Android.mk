057ecbd37b19bd2cca8a53f147d1ae85af8aa801
qwow5 <quint@quint5.tech>
2020-05-14 13:19:52 -0400

    Unlock Clock is now platform signed.

--- UnlockClock/Android.mk
+++ UnlockClock/Android.mk
@@ -2,8 +2,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := UnlockClock
-LOCAL_SRC_FILES := com.betterlineage.unlockclock_2.apk
+LOCAL_CERTIFICATE := platform
+
+LOCAL_SRC_FILES := unlockclock_unsigned.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
