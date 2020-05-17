c96ba63cd9f0807c8baa45983bf7e9b2a30e196b
Travis CI <deploy@travis-ci.org>
2020-05-17 03:05:29 +0200

    ci - update application MuPDFViewer with com.artifex.mupdf.viewer.app

--- MuPDFViewer/Android.mk
+++ MuPDFViewer/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := MuPDFViewer
-LOCAL_SRC_FILES := com.artifex.mupdf.viewer.app_70.apk
+LOCAL_SRC_FILES := com.artifex.mupdf.viewer.app_80.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
