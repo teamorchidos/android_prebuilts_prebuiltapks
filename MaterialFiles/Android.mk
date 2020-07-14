160e1cb4fd1f0d7080a3c30c3d49aceffdd97f36
Travis CI <deploy@travis-ci.org>
2020-07-14 03:40:32 +0200

    ci - update application MaterialFiles with me.zhanghai.android.files

--- MaterialFiles/Android.mk
+++ MaterialFiles/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := MaterialFiles
-LOCAL_SRC_FILES := me.zhanghai.android.files_19.apk
+LOCAL_SRC_FILES := me.zhanghai.android.files_20.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
