4a7d02321c22434cd69dad00785d8152377d8e76
Travis CI <deploy@travis-ci.org>
2020-07-30 03:49:51 +0200

    ci - update application OpenCamera with net.sourceforge.opencamera

--- OpenCamera/Android.mk
+++ OpenCamera/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := OpenCamera
-LOCAL_SRC_FILES := net.sourceforge.opencamera_77.apk
+LOCAL_SRC_FILES := net.sourceforge.opencamera_78.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
